e8209b304654090103d941e912649481d9e423e4
StephenLe <62473020+StephenLe31415@users.noreply.github.com>
2024-11-21 12:12:53 -0500

    Add #include statement

--- functions.h
+++ functions.h
@@ -1,12 +1,18 @@
-// User written functions
+#include <stdlib.h>
+
+//Define your struct here
 
 // Initializes the two user LEDs
 void init_user_leds();
+
 //Sets the two user LEDs
 void set_user_leds(unsigned char uled);
+
 // Initializes the buttons for input
 void init_buttons();
+
 //Reads the four buttons
 unsigned char read_buttons();
+
 //Turn on the buzzer
 void buzzer_on(int frequency);
