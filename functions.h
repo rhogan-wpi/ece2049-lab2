21b0e877c40b5dbede24f07382be808edd0ba093
StephenLe <62473020+StephenLe31415@users.noreply.github.com>
2024-11-22 12:13:20 -0500

    Added pseudocode for the ISR and note_duration()

--- functions.h
+++ functions.h
@@ -22,3 +22,11 @@ unsigned char read_buttons();
 
 //Turn on the buzzer
 void buzzer_on(int frequency);
+
+//Sets a specific note duration to the TimerA2
+//Assumption: A2 timer, Div1, and Up mode
+void note_duration(int duration) {
+	TA2CTL1 = TASSEL1 + ID_0 + MC_1;
+	TA2CCRO = duration - 1; //assume that we already converted seconds to ticks
+	TA2CCTL0 = CCIE;
+}
