e792c4ce7f93de172bdedd178124113df6eae813
rye-hogan <rhogan@wpi.edu>
2024-11-24 16:31:04 -0500

    Testing functions

--- functions.h
+++ functions.h
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 
 //note struct defintion
-struct note {
-    int duration; //in ticks
-    int pitch; //in ticks
-};
+//struct note {
+///int duration; //in ticks
+///    int pitch; //in ticks
+//};
 
-typedef note Note;
+//typedef note Note;
 
 // Initializes the two user LEDs
 void init_user_leds();
@@ -25,8 +25,8 @@ void buzzer_on(int frequency);
 
 //Sets a specific note duration to the TimerA2
 //Assumption: A2 timer, Div1, and Up mode
-void note_duration(int duration) {
-	TA2CTL1 = TASSEL1 + ID_0 + MC_1;
-	TA2CCRO = duration - 1; //assume that we already converted seconds to ticks
-	TA2CCTL0 = CCIE;
-}
+//void note_duration(int duration) {
+//	TA2CTL1 = TASSEL1 + ID_0 + MC_1;
+//	TA2CCRO = duration - 1; //assume that we already converted seconds to ticks
+//	TA2CCTL0 = CCIE;
+//}
