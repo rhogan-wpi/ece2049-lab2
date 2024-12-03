09b1fee8c83de130e26154cf31292c9dd5d598ed
StephenLe <62473020+StephenLe31415@users.noreply.github.com>
2024-12-03 17:21:23 -0500

    Fixed pitch and duration defintions.

--- functions.c
+++ functions.c
@@ -1,33 +1,4 @@
 #include "functions.h"
-// Pitch
-#define C4 125
-#define C4# 117
-#define D4 110
-#define D4# 104
-#define E4 99
-#define F4 93
-#define F4# 88
-#define G4 83
-#define Ab4 78
-#define A4 73
-#define B4b 69
-#define B4 65
-#define C5 62
-#define C5# 58
-#define D5 55
-#define E5b = 52
-#define E5 49
-#define F5 46
-#define F5# 43
-#define G5 41
-#define A5b 38
-#define A5 36
-
-// Duration
-#define whole 1000
-#define half 500
-#define quar 250
-#define eight 125
 // User written functions
 
 // Set LED function for note display
